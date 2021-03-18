b6771666f038fc0f9b1694156b3c1d837982bef8
LAPTOP-6B40E7JD\samgl <samglassman@protonmail.com>
2021-03-18 21:19:10 +0100

    added some controls to the gui

--- src/Testing.java
+++ src/Testing.java
@@ -12,7 +12,7 @@ public class Testing {
         Vector3d startingVel = new Vector3d(0,0,0);
         //2.147483647E9;
 
-        for (int i = 0; i < 50000; i++) {
+        for (int i = 0; i < 5000; i++) {
             SpaceObjectBuilder builder = new SpaceObjectBuilder("src/solar_system_data-2020_04_01.txt");
             ArrayList<SpaceObject> spaceObjects = new ArrayList<>(builder.getSpaceObjects());
             ProbeSimulator sim = new ProbeSimulator(spaceObjects);
