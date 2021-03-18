6542c9b9416a22ab243cd20158f2951e1e713c1d
LAPTOP-6B40E7JD\samgl <samglassman@protonmail.com>
2021-03-18 10:03:40 +0100

    started probe stuff

--- src/Main.java
+++ src/Main.java
@@ -24,16 +24,16 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(anchorPane));
         primaryStage.show();
         //initialiseCanvas(canvas);
-        ArrayList <SpaceObject> planets = new ArrayList<>();
-        Planet earth = new Planet("earth", 1000, new Vector3d(50,100,300), new Vector3d(0,0.0,0.0),70);
-        Planet mars = new Planet("mars", 400, new Vector3d(400,75,87), new Vector3d(0.0,0.0,0.0),80);
-        Planet saturn = new Planet("saturn", 3000, new Vector3d(50,400,640), new Vector3d(0.0,0.0,0.0),1000);
-        Planet uranus = new Planet("uranus", 2700, new Vector3d(97,280,90), new Vector3d(0.0,0.0,0.0),800);
-        Planet sun = new Planet("sun", 27000000, new Vector3d(100,100,20), new Vector3d(0.0,0.0,0.0),800);
+        ArrayList<SpaceObject> planets = new ArrayList<>();
+        Planet earth = new Planet("earth", 5.97219e24, new Vector3d(-1.471922101663588e+11, -2.860995816266412e+10, 8.278183193596080e+06), new Vector3d(5.427193405797901e+03, -2.931056622265021e+04, 6.575428158157592e-01), 70);
+//        Planet mars = new Planet("mars", 400, new Vector3d(400, 75, 87), new Vector3d(0.0, 0.0, 0.0), 80);
+//        Planet saturn = new Planet("saturn", 3000, new Vector3d(50, 400, 640), new Vector3d(0.0, 0.0, 0.0), 1000);
+//        Planet uranus = new Planet("uranus", 2700, new Vector3d(97, 280, 90), new Vector3d(0.0, 0.0, 0.0), 800);
+        Planet sun = new Planet("sun", 1.988500e30, new Vector3d(-6.806783239281648e+08, 1.080005533878725e+09, 6.564012751690170e+06), new Vector3d(-1.420511669610689e+01, -4.954714716629277e+00, 3.994237625449041e-01), 800);
         planets.add(earth);
-        planets.add(mars);
-        planets.add(saturn);
-        planets.add(uranus);
+//        planets.add(mars);
+//        planets.add(saturn);
+//        planets.add(uranus);
         planets.add(sun);
 
         //StateInterface state = new State();
@@ -49,34 +49,44 @@ public class Main extends Application {
             velocities[i] = spaceObject.getVelocity();
             mass[i++] = spaceObject.getMass();
         }
+//        for (int i = 0; i < planets.size(); i++){
+//            positions[i] = new Vector3d(planets.get(i).getPosition().getX(),planets.get(i).getPosition().getY(),planets.get(i).getPosition().getZ());
+//            velocities[i] = new Vector3d(planets.get(i).getPosition().getX(),planets.get(i).getPosition().getY(),planets.get(i).getPosition().getZ());
+//
+//        }
 
+        ProbeSimulator sim = new ProbeSimulator(spaceObjects);
+        System.out.println(Arrays.toString(sim.trajectory(new Vector3d(0, 0, 0), new Vector3d(60, 0, 0),31556926, 1000)));
+
+
+
+
+
+        //create the initial state
         State state = new State(positions, velocities, 0);
         State.setMass(mass);
         State.setNames();
 
-//TODO where did the minuses go????
         System.out.println(state.toString());
+       ODEFunction f = new ODEFunction();
 
+        //    State state1 = (State) state.addMul(1, f.call(0 + 1, state));
+        // System.out.println(state1.toString());
 
-        ODEFunction f = new ODEFunction();
-
-        State state1 = (State) state.addMul(1, f.call(0 + 1, state));
-       // System.out.println(state1.toString());
-
-
+ /*
         double[] ts = new double[]{0, 31556926};
         Solver x = new Solver();
-        StateInterface[] s = x.solve(f, state1, ts);
-        System.out.println( s[1] );
-        System.out.println();
+     //   StateInterface[] s = x.solve(f, state, ts);
+     //   System.out.println(s[1]);
+     //   System.out.println();
 
         double tf = 31556926;
-        StateInterface[] s1 = x.solve(f, state1, tf, 1000);
-        System.out.println( s1[0] );
-        System.out.println();
+        StateInterface[] s1 = x.solve(f, state, tf, 10000);
 
+        System.out.println(s1[s1.length - 1]);
+*/
 
-        Renderer renderer = new Renderer(canvas,planets, state);
+        Renderer renderer = new Renderer(canvas, planets, state);
 
         renderer.start();
 
@@ -84,8 +94,8 @@ public class Main extends Application {
     }
 
     private void initialiseCanvas(Canvas canvas) {
-        canvas.widthProperty().bind(((AnchorPane)canvas.getParent()).widthProperty());
-        canvas.heightProperty().bind(((AnchorPane)canvas.getParent()).heightProperty());
+        canvas.widthProperty().bind(((AnchorPane) canvas.getParent()).widthProperty());
+        canvas.heightProperty().bind(((AnchorPane) canvas.getParent()).heightProperty());
     }
 
 
