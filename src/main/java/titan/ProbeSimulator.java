4a5253299c86a020c2a59325d887abaa2bdde9be
Lou1108 <72265570+Lou1108@users.noreply.github.com>
2021-05-05 17:09:33 +0200

    hi

--- src/main/java/titan/ProbeSimulator.java
+++ src/main/java/titan/ProbeSimulator.java
@@ -88,7 +88,7 @@ public class ProbeSimulator implements ProbeSimulatorInterface {
         State universe = initUn();
 
         //creates all the states of the simulation
-        Solver solver = new Solver(new VerletSolver());
+        Solver solver = new Solver(new EulerSolver());
       //  tf=h*2000; //custom 5 step
         StateInterface[] s = solver.solve(new ODEFunction(), universe, tf, h);
         states=s;
