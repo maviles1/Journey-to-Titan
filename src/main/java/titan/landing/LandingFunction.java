26a04a7cb2e1f21c9ed8ae3bbe297c77cf7d0fdc
Tjardo <44730725+Whatsoutside@users.noreply.github.com>
2021-06-10 17:47:47 +0200

    updated landing function

--- src/main/java/titan/landing/LandingFunction.java
+++ src/main/java/titan/landing/LandingFunction.java
@@ -1,18 +1,44 @@
 package titan.landing;
 
+import titan.flight.Rate;
+import titan.flight.State;
 import titan.flight.Vector3d;
+import titan.interfaces.RateInterface;
+import titan.interfaces.StateInterface;
 
-public class LandingFunction {
-    private double gravity = 1.352; //titan gravity m/s (ms-2)
+import java.util.Vector;
+
+public class LandingFunction implements RateInterface {
+    private double titanforce = 1.352; //titan gravity m/s (ms-2)
     private double distance; //probe to titans distance
+    private final Vector3d titanpos = new Vector3d(0,0,0);
+    private Vector3d gravity;
 
-
-    public Vector3d getGravityVector(Shuttle shuttle)
+    public Vector3d getGravityVector(Vector3d shuttle) //shuttles position
     {
-        Vector3d titan = new Vector3d(0,0,0);
-        return new Vector3d(1, 1, 1);
+        distance = shuttle.dist(titanpos);
+        gravity = titanpos.sub(shuttle);
+        gravity = gravity.mul(titanforce/gravity.norm());
+
+        return new Vector3d(gravity.getX(), gravity.getY(), gravity.getZ());
     }
 
+    //wind
 
+    //thruster
+
+    public RateInterface call(double t, StateInterface y)
+    {
+        State state = (State)y;
+        double dt = t - state.getTime();
+        Vector3d change = getGravityVector(state.getPositions()[0]); // Gravity + wind + thruster
+
+        Vector3d[] velocity = new Vector3d[1];
+        Vector3d[] acceleration = new Vector3d[1];
+
+        acceleration[0] = change;
+
+        return new Rate(velocity, acceleration);
+    }
 
 }
