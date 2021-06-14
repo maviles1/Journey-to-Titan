e6917296042a29878464b90fcf1fe46595313585
Tjardo <44730725+Whatsoutside@users.noreply.github.com>
2021-06-14 18:18:58 +0200

    Updated windmodel

--- src/main/java/titan/landing/WindModel.java
+++ src/main/java/titan/landing/WindModel.java
@@ -43,14 +43,19 @@ public class WindModel {
     public void CalculateWindRotation(Vector3d windvector, Shuttle sh)
     {
         // Select a random point where it hits the probe (on the y-axis)
-        double max = sh.getPosition().getY() + (sh.getHeight()/2);
-        double min = sh.getPosition().getY() - (sh.getHeight()/2);
-        double newY = (max-min) * gen.nextDouble() + min;
+        double radius = sh.getHeight()/2;
+        double Ymax = sh.getPosition().getY() + radius;
+        double Ymin = sh.getPosition().getY() - radius;
+        double Xmax = sh.getPosition().getX() + radius;
+        double Xmin = sh.getPosition().getX() - radius;
+        double newY = (Ymax-Ymin) * gen.nextDouble() + Ymin;
+        double newX = (Xmax-Xmin) * gen.nextDouble() + Xmin;
 
         // Point where the wind hits the probe
-        Vector3d windimpact = new Vector3d(sh.getPosition().getX(), newY, sh.getPosition().getZ());
+        Vector3d windimpact = new Vector3d(newX, newY, sh.getPosition().getZ());
 
-        // Calculate the strength of the wind vector in rotations
+        // Calculate the strength of the wind vector in rotations per timestep
+
 
     }
 
