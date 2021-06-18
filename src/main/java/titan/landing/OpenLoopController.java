332c59de17579b6ea5396194f035d6f1fc9416bd
mariepicquet <ngf>
2021-06-18 16:43:07 +0200

    dd

--- src/main/java/titan/landing/OpenLoopController.java
+++ src/main/java/titan/landing/OpenLoopController.java
@@ -34,26 +34,35 @@ public class OpenLoopController implements Controller {
         double mainThrust = 1;
         double xAccel = mainThrust * Math.sin(state.getAngle());
         double yAccel = mainThrust * Math.cos(state.getAngle());
-        double angularAccel = 0; //torque TODO: calculate torque
+        double angularAccel = angularAcceleration(torque(new Vector3d(2,2,0), state));
 
         //
 
         return new LandingRate(state.getVelocity(), thrust, state.getShuttle_direction(), state.getWind_direction(), angularAccel);
     }
 
-    public Vector3d torque(Vector3d force, LandingState state) {
-        //FOR LEFT THRUSTER
-        //I = 1/2 * mass * radius^2 TODO: maybe we should take the moment of inertia of a sphere 2/3 * m * r^2 (hollow) or 2/5 * m * r^2 (solid)
-        //double momentInertia = 0.5 * MASS * RADIUS * RADIUS;
+    public Double torque(Vector3d force, LandingState state) {
+//        //FOR LEFT THRUSTER
+//        //I = 1/2 * mass * radius^2 TODO: maybe we should take the moment of inertia of a sphere 2/3 * m * r^2 (hollow) or 2/5 * m * r^2 (solid)
+//        //double momentInertia = 0.5 * MASS * RADIUS * RADIUS;
         double thrustAngle = 45;
-        double angle = 0;
-        //Vector3d torque = force.mul(RADIUS * Math.sin(thrustAngle));
-        Vector3d thrusterPos = new Vector3d(state.getPosition().getX() - RADIUS, state.getPosition().getY(), 0);
-        Vector3d r = thrusterPos.sub(state.getPosition());
-        //r = rotate(state.getAngle(), r);
-        angle = Math.toDegrees(Math.acos(r.dot(force)/(force.norm()*r.norm()))); //angle between force and position in degrees! TODO: is it always 45°?
+//        double angle = 0;
+//        //Vector3d torque = force.mul(RADIUS * Math.sin(thrustAngle));
+//        Vector3d thrusterPos = new Vector3d(state.getPosition().getX() - RADIUS, state.getPosition().getY(), 0);
+//        Vector3d r = thrusterPos.sub(state.getPosition());
+//        //r = rotate(state.getAngle(), r);
+//        angle = Math.toDegrees(Math.acos(r.dot(force)/(force.norm()*r.norm()))); //angle between force and position in degrees! TODO: is it always 45°?
+//
+//        return force.cross(r); //TODO: check if have to take the angle into account?
+        force = rotate(state.getAngle(), force);
+        double forceMagnitude = force.norm();
+        return forceMagnitude*RADIUS*Math.sin(thrustAngle);
 
-        return force.cross(r); //TODO: check if have to take the angle into account?
+    }
+
+    public double angularAcceleration(double torque) {
+        double momentInertia = 0.5 * MASS * RADIUS * RADIUS;
+        return torque/momentInertia;
     }
 
     public Vector3d rotate(double angle, Vector3d vector) {
